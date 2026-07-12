df1e96683da901a0383d24c15b68aee3eca5e1f4
SirRiuz <mj9220019@gmail.com>
2026-07-12 13:12:12 -0500

    foo

--- app/constants/search.py
+++ app/constants/search.py
@@ -26,3 +26,9 @@ SUGGEST_TAGS_LIMIT = 5  # tags while typing (prefix, by trend score)
 SUGGEST_THREADS_LIMIT = 4  # threads while typing (content, by momentum)
 SUGGEST_TRENDING_LIMIT = 8  # trending tags for the empty input
 SUGGEST_SNIPPET_RADIUS = 30  # characters on each side of the match
+
+print('dddd')
+
+
+
+
